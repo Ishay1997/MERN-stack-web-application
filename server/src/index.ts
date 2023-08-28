e292fe77aa329c0f9e21687ea9a1b700e67e2c56
ishay1997 <ishaylevy8@gmail.com>
2023-08-28 18:39:12 +0300

    working on showing the decks on the landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,27 @@
+import { config } from 'dotenv';
+config();
 import express, { Request, Response } from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
+
 import Deck from "./models/Deck";
 
 const PORT = 5000;
 const app = express();
+app.use(cors({origin: "*",}));
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
-
+app.get("/decks",async (req: Request, res: Response) => {
+const decks = await Deck.find();
+res.json(decks);
+});
 // Define a POST route to create a new deck
 app.post("/decks", async (req: Request, res: Response) => {
     try {
-        const newDeck = new Deck({ title: 'my awesome flashcard deck' });
+        const newDeck = new Deck({ title: req.body.title, });
         const createDeck = await newDeck.save();
-        res.status(201).json(newDeck);
+        res.json(createDeck);
     } catch (error) {
         console.error("Error creating deck:", error);
         res.status(500).json({ error: "Internal server error" });
@@ -36,7 +44,7 @@ app.get("/hello", (req: Request, res: Response) => {
 });
 
 // Connect to MongoDB and start the server
-mongoose.connect("mongodb+srv://flashcardsage:okOtPs8t45GKm9Qo@cluster0.ko5cio7.mongodb.net/test").then(() => {
+mongoose.connect("mongodb+srv://flashcardsage:YCd3TXmDrIt7lQRe@cluster0.ko5cio7.mongodb.net/?retryWrites=true&w=majority").then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
         console.log(`Server is listening on port ${PORT}`);
@@ -45,11 +53,4 @@ mongoose.connect("mongodb+srv://flashcardsage:okOtPs8t45GKm9Qo@cluster0.ko5cio7.
     console.error("Error connecting to MongoDB:", error);
 });
 
-// Assuming you have a model defined for "Deck" in a separate file
-// models/Deck.js
-// const mongoose = require("mongoose");
-// const deckSchema = new mongoose.Schema({
-//     title: String
-// });
-// const Deck = mongoose.model("Deck", deckSchema);
-// module.exports = Deck;
+
